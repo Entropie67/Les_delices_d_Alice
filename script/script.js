b9850331697f40055e43bbd27a312428382726cd
Olivier <olivier.elophe@gmail.com>
2023-12-03 23:25:17 +0100

    Exemple de manipulation des recettes

--- script/script.js
+++ script/script.js
@@ -3,8 +3,8 @@
 const recettes = recipes;
 // Affichage de la première recette dans la console du navigateur :
 console.log("Première recette : ", recettes[0]);
-// Bien observer  la structure, le JSON en JS ressemble beaucoup aux dictionnaires Pythons,
-// Par exemple pour obtenir le tableau (encore!) des ingrédients de la recette 1 :
+// Bien observer la structure, le JSON en JS ressemble beaucoup aux dictionnaires Python,
+// Par exemple pour obtenir le tableau (encore !) des ingrédients de la recette 1 :
 console.log("Ingrédients de la première recette : ", recettes[0]["ingredients"]);
 // Le premier de ces ingrédients :
 console.log("Premier ingrédient, de la première recette : ", recettes[0]["ingredients"][0]);
