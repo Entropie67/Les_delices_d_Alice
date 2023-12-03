1fb50624c42004269fd104a866b9254f18b2abb2
Olivier <olivier.elophe@gmail.com>
2023-12-03 23:17:52 +0100

    Exemple de manipulation des recettes

--- script/script.js
+++ script/script.js
@@ -1,4 +1,15 @@
 
-
+// L'ensemble des recettes se trouve dans cette variable recette
 const recettes = recipes;
-console.log(recettes);
+// Affichage de la première recette dans la console du navigateur :
+console.log("Première recette : ", recettes[0]);
+// Bien observer  la structure, le JSON en JS ressemble beaucoup aux dictionnaires Pythons,
+// Par exemple pour obtenir le tableau (encore!) des ingrédients de la recette 1 :
+console.log("Ingrédients de la première recette : ", recettes[0]["ingredients"]);
+// Le premier de ces ingrédients :
+console.log("Premier ingrédient, de la première recette : ", recettes[0]["ingredients"][0]);
+console.log("Premier ingrédient, de la première recette : ", recettes[0]["ingredients"][0].ingredient);
+// en JS on peut soit faire ["ingredient"], comme en Python, ou faire .ingredient
+
+// A vous de jouer !!
+/****************************************************************************************************/
